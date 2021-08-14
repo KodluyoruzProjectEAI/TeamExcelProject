2eaf899b4631d7f416302480b9c7f368857c0475
hakaell <hakael_37@hotmail.com>
2021-08-14 15:54:53 +0300

    Add Animation's Script
    Add  AnimationController

---
+++ BitirmeProjesi/Assets/Player/Scripts/AnimationController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace Player.Scripts
+{
+   public class AnimationController : MonoBehaviour
+   {
+      public Animator animator;
+      private static readonly int RunningID = Animator.StringToHash("Running");
+      private static readonly int IdleID = Animator.StringToHash("idle");
+      private static readonly int InjuredRunID = Animator.StringToHash("InjuredRun");
+      private static readonly int DeadID = Animator.StringToHash("DeadID");
+      private static readonly int DanceID = Animator.StringToHash("Dance");
+      private static readonly int SlideRunID = Animator.StringToHash("SlideRunID");
+
+      public void Idle()
+      {
+         animator.SetTrigger(IdleID);
+      }
+      public void Run()
+      {
+         animator.SetTrigger(RunningID);
+      }
+      public void InjuredRun()
+      {
+         animator.SetTrigger(InjuredRunID);
+      }
+      public void Dead()
+      {
+         animator.SetTrigger(DeadID);
+      }
+      public void Dance()
+      {
+         animator.SetTrigger(DanceID);
+      }
+      public void SlideRun()
+      {
+         animator.SetTrigger(SlideRunID);
+      }
+   }
+}
