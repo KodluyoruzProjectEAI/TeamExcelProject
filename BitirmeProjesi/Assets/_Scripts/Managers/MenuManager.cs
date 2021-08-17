9310707f1caf317b54fe385720c0e38513bed949
İlker <36369727+ilkerkanber@users.noreply.github.com>
2021-08-17 21:23:35 +0300

    Merge

--- BitirmeProjesi/Assets/_Scripts/Managers/MenuManager.cs
+++ BitirmeProjesi/Assets/_Scripts/Managers/MenuManager.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
 
 public class MenuManager : ASingleton<MenuManager>
 {
+    public AManager aManager;
+    public GameObject canvas;
     void Awake()
     {
         StartSingleton(this);
+    }
+    private void Start()
+    {
+        aManager = FindObjectOfType<AManager>();
+
+    }
+    public void Play()
+    {
+        //Player Manager'dan start methodundaki statei IDLE'a aldým, tap to Play yaptýktan sonra runninge geçsin istedim, geçmiyor
+        aManager.currentState = AManager.State.Running;
+        canvas.SetActive(false);
+        Debug.Log("ÇALIÞ LÜTFN");
     }
 }
